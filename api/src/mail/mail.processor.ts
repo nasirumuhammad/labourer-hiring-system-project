df4a158c1e6f7aacfa452ab888895db1747cce15
muhammad <muhammadnasiru9292@gmail.com>
2026-09-11 20:47:22 +0100

    feat:token added token service to the api

--- api/src/mail/mail.processor.ts
+++ api/src/mail/mail.processor.ts
@@ -1,20 +1,73 @@
 import { SystemJobs } from '@/common/constants/system-jobs.constant';
 import { SystemQueues } from '@/common/constants/system-queue.constant';
 import { Processor, WorkerHost } from '@nestjs/bullmq';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Job } from 'bullmq';
+import { maskEmail } from '@/auth/common/utils/mask.util';
 import { Mailservice } from './mail.service';
 
 @Injectable()
 @Processor(SystemQueues.EMAIL)
 export class MailProcessor extends WorkerHost {
+  private readonly logger = new Logger(MailProcessor.name);
+
   constructor(private readonly mailService: Mailservice) {
     super();
   }
-  process(job: Job, token?: string): Promise<any> {
+
+  async process(job: Job, token?: string): Promise<any> {
     const { email, otp } = job.data;
+
     try {
-      await this.mailService.sendForgotPasswordEmail({ email, otp }, '');
-    } catch (error: any) {}
+      switch (job.name) {
+        case SystemJobs.SEND_FORGOT_PASSWORD_OTP:
+          await this.mailService.sendForgotPasswordEmail(
+            { email, otp },
+            'Reset your password',
+          );
+
+          this.logger.log(
+            {
+              jobId: job.id,
+              jobName: job.name,
+              email: maskEmail(email),
+            },
+            `Processed ${job.name} job`,
+          );
+          break;
+        case SystemJobs.RESEND_OTP:
+          await this.mailService.sendForgotPasswordEmail(
+            { email, otp },
+            'Reset your password',
+          );
+
+          this.logger.log(
+            {
+              jobId: job.id,
+              jobName: job.name,
+              email: maskEmail(email),
+            },
+            `Processed ${job.name} job`,
+          );
+          break;
+        default:
+          this.logger.warn(
+            { jobId: job.id, jobName: job.name },
+            `Unknown job name received on ${SystemQueues.EMAIL}`,
+          );
+      }
+    } catch (error: any) {
+      this.logger.error(
+        {
+          jobId: job.id,
+          jobName: job.name,
+          email: maskEmail(email),
+          err: error,
+        },
+        `Failed to process ${job.name} job`,
+      );
+
+      throw error;
+    }
   }
 }
